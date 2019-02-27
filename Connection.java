05b55395295dceef060ae69749561cc3d8300cd2
Gazal Choudhary <47411483+GazalChoudhary@users.noreply.github.com>
2019-02-26 19:04:56 -0800

    Add files via upload

---
+++ Connection.java
@@ -0,0 +1,24 @@
+import java.beans.Statement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+public class Connection {
+	public static void main(String args[]) throws SQLException {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con=(Connection) DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","Gazal","gazal18");
+			Statement smt=con.createStatement();
+			String sql="select * from customer";
+			ResultSet rs=smt.executeQuery(sql);
+			while(rs.next())
+				System.out.println(rs.getInt(1)+ "  "+rs.getString(2)+ " "+rs.getString(3);
+			con.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
